3570eaff91bbcb3ec7b55af2e70c1472b7243788
Hannes <hasnep@gmail.com>
2019-09-18 20:25:01 +0100

    Fix motion to rotate on the correct axes

--- Tank/Tank.gd
+++ Tank/Tank.gd
@@ -1,24 +1,27 @@
 extends KinematicBody
 
-const max_speed: float = 20.0
-const acceleration: float = 10.0
+const max_speed: float = 1.0
+const acceleration: float = 0.01
 
 var direction: float = 0.0
 var velocity: Vector3 = Vector3()
+var speed: float = 0.0
 
 func _physics_process(delta: float):
-
 	var input_forward: float = 0.0
 	var input_rotation: float = 0.0
 
 	if Input.is_action_pressed("move_forward"):
-		input_forward += 1
+		if speed < max_speed:
+			speed += acceleration
+	else:
+		speed /= 2
 	if Input.is_action_pressed("move_backward"):
-		input_forward -= 1
+		speed -= acceleration
 	if Input.is_action_pressed("turn_left"):
-		input_rotation -= 1
-	if Input.is_action_pressed("turn_right"):
 		input_rotation += 1
+	if Input.is_action_pressed("turn_right"):
+		input_rotation -= 1
 
-	rotation_degrees += Vector3(input_rotation, 0, 0)
-	move_and_slide(Vector3(input_forward, 0, 0), Vector3(0, 0, 0))
+	rotation_degrees += Vector3(0, input_rotation, 0)
+	move_and_slide(speed * global_transform.basis.z)
