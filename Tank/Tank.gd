cf0ec29ced01dedec69ab31e22f80c967cdd9c4a
Hannes <hasnep@gmail.com>
2019-09-29 19:09:24 +0100

    Add 8 direction keyboard controls

--- Tank/Tank.gd
+++ Tank/Tank.gd
@@ -1,27 +1,34 @@
 extends KinematicBody
 
 const max_speed: float = 10.0
-const acceleration: float = 0.1
+const max_angular_velocity: float = 0.1
 
-var direction: float = 0.0
-var velocity: Vector3 = Vector3()
 var speed: float = 0.0
 
 func _physics_process(delta: float):
-	var input_forward: float = 0.0
-	var input_rotation: float = 0.0
+    var desired_z: int = 0
+    var desired_x: int = 0
 
-	if Input.is_action_pressed("move_forward"):
-		if speed < max_speed:
-			speed += acceleration
-	else:
-		speed /= 2
-	if Input.is_action_pressed("move_backward"):
-		speed -= acceleration
-	if Input.is_action_pressed("turn_left"):
-		input_rotation += 1
-	if Input.is_action_pressed("turn_right"):
-		input_rotation -= 1
+    if Input.is_action_pressed("move_up"):
+        desired_z -= 1
+    if Input.is_action_pressed("move_down"):
+        desired_z += 1
+    if Input.is_action_pressed("move_left"):
+        desired_x -= 1
+    if Input.is_action_pressed("move_right"):
+        desired_x += 1
 
-	rotation_degrees += Vector3(0, input_rotation, 0)
-	move_and_slide(speed * global_transform.basis.z)
+    var direction_delta: float
+    if desired_x == 0 & & desired_z == 0:
+        speed = 0
+    else:
+        var facing: Vector2 = Vector2(global_transform.basis.z.x, global_transform.basis.z.z)
+        direction_delta = Vector2(desired_x, desired_z).angle_to(facing)
+        # Limit rotation speed
+        direction_delta = min(direction_delta, max_angular_velocity)
+        direction_delta = max(direction_delta, -max_angular_velocity)
+        if abs(direction_delta) < 0.001:
+            direction_delta = 0
+        rotation += Vector3(0, direction_delta, 0)
+        speed = max_speed
+    move_and_slide(speed * global_transform.basis.z)
