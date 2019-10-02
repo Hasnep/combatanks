59fed90344401ede90d0dc938f5cb423cd87fc4f
Hannes <hasnep@gmail.com>
2019-10-02 07:39:18 +0100

    Split camera and GUI into separate scene

--- Camera/Camera.gd
+++ Camera/Camera.gd
@@ -1,6 +1,7 @@
 extends Camera
 
 onready var mouse_marker = $"../MouseMarker"
+onready var cursor = $"GUI/Cursor"
 
 var floor_plane: Plane = Plane(Vector3(0, 1, 0), 0)
 
@@ -8,3 +9,5 @@ func _process(delta:float):
 	var mouse_viewport: Vector2 = get_viewport().get_mouse_position()
 	var mouse_world: Vector3 = floor_plane.intersects_ray(project_ray_origin(mouse_viewport), project_ray_normal(mouse_viewport))
 	mouse_marker.transform.origin = mouse_world
+	
+	cursor.position=mouse_viewport	
