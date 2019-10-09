eccd4158766390fe442aa84b5642564c8fb1074f
Hannes <hasnep@gmail.com>
2019-10-09 08:36:46 +0100

    Add ability to shoot bullets

--- Bullet/Bullet.gd
+++ Bullet/Bullet.gd
@@ -1,4 +1,13 @@
 extends KinematicBody
 
-func _physics_process(delta:float):
-	pass
+const speed: float = 10.0
+
+func _physics_process(delta: float):
+	move_and_slide(speed * global_transform.basis.z)
+
+func constructor(parent: KinematicBody, turret_position: Vector3, direction: Vector3):
+	add_collision_exception_with(parent) # don't collide with parent
+	rotation = direction
+	transform.origin = turret_position + \
+	Vector3(0,1.5,0) + \ # add turret height
+	Vector3()
