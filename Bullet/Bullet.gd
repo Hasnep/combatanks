2773b56a25ac70ae459a796b24b5205adc7f6835
Hannes <hasnep@gmail.com>
2020-03-21 23:50:07 +0000

    Format code using autopep8

--- Bullet/Bullet.gd
+++ Bullet/Bullet.gd
@@ -6,8 +6,8 @@ func _physics_process(delta: float):
 	move_and_slide(speed * global_transform.basis.z)
 
 func constructor(parent: KinematicBody, turret_position: Vector3, direction: Vector3):
-	add_collision_exception_with(parent) # don't collide with parent
+	add_collision_exception_with(parent)  # don't collide with parent
 	rotation = direction
-	turret_position += Vector3(0,1.5,0) # add turret height 
-	turret_position += Vector3() # add barrel length
+	turret_position += Vector3(0, 1.5, 0)  # add turret height
+	turret_position += Vector3()  # add barrel length
 	transform.origin = turret_position
--- Tank/TankBase.gd
+++ Tank/TankBase.gd
@@ -10,7 +10,7 @@ var speed: float = 0.0
 func _physics_process(delta: float):
 	var desired_z: int = 0
 	var desired_x: int = 0
-	
+
 	if Input.is_action_pressed("move_up"):
 		desired_z -= 1
 	if Input.is_action_pressed("move_down"):
@@ -19,23 +19,23 @@ func _physics_process(delta: float):
 		desired_x -= 1
 	if Input.is_action_pressed("move_right"):
 		desired_x += 1
-	
+
 	if desired_x == 0 and desired_z == 0:
 		speed = 0
 	else:
 		var facing: Vector2 = Vector2(global_transform.basis.z.x, global_transform.basis.z.z)
 		var direction_delta: float = Vector2(desired_x, desired_z).angle_to(facing)
-		
+
 		# Limit rotation speed
 		direction_delta = min(direction_delta, max_angular_velocity)
 		direction_delta = max(direction_delta, -max_angular_velocity)
-		
+
 		# Snap to direction
 		if abs(direction_delta) < 0.001:
 			direction_delta = 0
-		
+
 		rotation += Vector3(0, direction_delta, 0)
 		speed = max_speed
-	
+
 	# Move tank
 	move_and_slide(speed * global_transform.basis.z)
