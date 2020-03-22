a583b7ba3a4d423bbaa9aa6d082d9bfc38b0ff1b
Hannes <hasnep@gmail.com>
2020-03-22 15:44:32 +0000

    Refactor bullet spawn position

--- Bullet/Bullet.gd
+++ Bullet/Bullet.gd
@@ -6,9 +6,11 @@ const BARREL_LENGTH: float = 1.8
 func constructor(parent: KinematicBody, turret_position: Vector3, direction: Vector3):
 	add_collision_exception_with(parent)  # Don't collide with parent
 	rotation = direction
-	turret_position += Vector3(0, 1.5, 0)  # Add turret height
-	turret_position += BARREL_LENGTH * transform.basis.z  # Add barrel length
-	transform.origin = turret_position
+	transform.origin = (
+		turret_position
+            + Vector3(0, 1.5, 0)  # Add turret height
+            + BARREL_LENGTH * transform.basis.z  # Add barrel length
+	)
 
 func _physics_process(_delta: float):
 	move_and_slide(BULLET_SPEED * global_transform.basis.z)
