cc11481e9d8210b72fd6191151e73e9dcadb9191
Hannes <hasnep@gmail.com>
2020-03-22 15:45:24 +0000

    Refactor turret height into a constant

--- Bullet/Bullet.gd
+++ Bullet/Bullet.gd
@@ -2,13 +2,14 @@ extends KinematicBody
 
 const BULLET_SPEED: float = 10.0
 const BARREL_LENGTH: float = 1.8
+const TURRRET_HEIGHT: float = 1.5
 
 func constructor(parent: KinematicBody, turret_position: Vector3, direction: Vector3):
 	add_collision_exception_with(parent)  # Don't collide with parent
 	rotation = direction
 	transform.origin = (
 		turret_position
-            + Vector3(0, 1.5, 0)  # Add turret height
+            + Vector3(0, TURRRET_HEIGHT, 0)  # Add turret height
             + BARREL_LENGTH * transform.basis.z  # Add barrel length
 	)
 
