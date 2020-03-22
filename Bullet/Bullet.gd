7c2406cfe8e2dfcd6a9176e13b427b710dbc192c
Hannes <hasnep@gmail.com>
2020-03-22 12:13:21 +0000

    Rename speed constant to be in capitals

--- Bullet/Bullet.gd
+++ Bullet/Bullet.gd
@@ -1,10 +1,10 @@
 extends KinematicBody
 
-const speed: float = 10.0
+const BULLET_SPEED: float = 10.0
 const BARREL_LENGTH: float = 1.8
 
 func _physics_process(delta: float):
-	move_and_slide(speed * global_transform.basis.z)
+	move_and_slide(BULLET_SPEED * global_transform.basis.z)
 
 func constructor(parent: KinematicBody, turret_position: Vector3, direction: Vector3):
 	add_collision_exception_with(parent)  # Don't collide with parent
