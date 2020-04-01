f3e0d357935ed0c166eb307481e825614ba378e1
Hannes <hasnep@gmail.com>
2020-04-01 13:24:37 +0100

    Add mine explosions

--- Mine/Mine.gd
+++ Mine/Mine.gd
@@ -1,4 +1,12 @@
 extends Spatial
 
+var MineExplosion: Resource = load("res://Mine/MineExplosion.tscn")
+
 func constructor(tank_position: Vector3):
 	transform.origin = tank_position
+
+func _on_timer_timeout():
+	var e = MineExplosion.instance()
+	get_parent().add_child(e)
+	e.transform.origin = self.transform.origin
+	self.queue_free()
---
+++ Mine/MineExplosion.gd
@@ -0,0 +1,57 @@
+extends Spatial
+
+onready var MineExplosionTween: Tween = $"MineExplosionTween"
+onready var MineExplosionParticles = $"MineExplosionParticles"
+onready var MineExplosionLight = $"MineExplosionLight"
+onready var MineExplosionModel: MeshInstance = $"MineExplosionModel"
+
+const EXPLOSION_DURATION: float = 1.0
+
+
+func _ready():
+	# Grow explosion sphere
+	MineExplosionTween.interpolate_property(
+		MineExplosionModel,
+		"scale",
+		Vector3(1, 1, 1),
+		Vector3(3, 3, 3),
+		EXPLOSION_DURATION / 2,
+		Tween.TRANS_LINEAR,
+		Tween.EASE_OUT
+	)
+	# Rotate explosion
+	MineExplosionTween.interpolate_property(
+		MineExplosionModel,
+		"rotation_degrees",
+		Vector3(0, 0, 0),
+		Vector3(0, 90, 0),
+		EXPLOSION_DURATION,
+		Tween.TRANS_LINEAR,
+		Tween.EASE_OUT
+	)
+	# Move time forwards for explosion animaiton
+	MineExplosionTween.interpolate_property(
+		MineExplosionModel.get_surface_material(0),
+		"shader_param/my_time",
+		0,
+		1,
+		EXPLOSION_DURATION,
+		Tween.TRANS_LINEAR,
+		Tween.EASE_OUT
+	)
+	# Increase light intensity
+	MineExplosionTween.interpolate_property(
+		MineExplosionLight,
+		"light_energy",
+		50,
+		0,
+		EXPLOSION_DURATION,
+		Tween.TRANS_LINEAR,
+		Tween.EASE_OUT
+	)
+	MineExplosionTween.start()
+	# Emit particles
+	MineExplosionParticles.emitting = true
+
+func _on_explosion_tweens_completed():
+	self.queue_free()
