931e730b13e96a56244104b201c90e3c73133471
Hannes <hasnep@gmail.com>
2020-04-01 18:27:54 +0100

    Add quit game button

--- Global.gd
+++ Global.gd
@@ -3,3 +3,5 @@ extends Node
 func _input(event:InputEvent):
 	if event.is_action_pressed("fullscreen_toggle"):
 		OS.window_fullscreen = !OS.window_fullscreen
+	if event.is_action_released("quit_game"):
+		get_tree().quit()
