9cd0801dd14718cf34782ba9de20cec417ba2f1f
Hannes <>
2020-04-30 22:43:45 +0100

    Make bullets bounce off walls

--- Bullet/Bullet.gd
+++ Bullet/Bullet.gd
@@ -1,13 +1,24 @@
 extends KinematicBody
 
-const BULLET_SPEED: float = 10.0
+const BULLET_SPEED: float = 0.1
 const BARREL_LENGTH: float = 1.8
 const TURRRET_HEIGHT: float = 1.5
+
+var velocity: Vector3
+
+
+func two_to_three(v: Vector2) -> Vector3:
+	return Vector3(v.x, 0, v.y)
+
+
+func three_to_two(v: Vector3) -> Vector2:
+	return Vector2(v.x, v.z)
 
 
 func constructor(parent: KinematicBody, turret_position: Vector3, direction: Vector3):
 	add_collision_exception_with(parent)  # Don't collide with parent
 	rotation = direction
+	velocity = BULLET_SPEED * transform.basis.z
 	transform.origin = (
 		turret_position
 		+ Vector3(0, TURRRET_HEIGHT, 0)
@@ -15,5 +26,29 @@ func constructor(parent: KinematicBody, turret_position: Vector3, direction: Vec
 	)
 
 
+func _collide_wall(collision: KinematicCollision):
+	var collision_normal: Vector3 = collision.normal
+	var collision_normal_2d: Vector2 = three_to_two(collision_normal)
+	# Calculate the remaining motion after colliding
+	var remaining_motion_2d: Vector2 = three_to_two(collision.remainder).bounce(collision_normal_2d)
+	var remaining_motion_3d: Vector3 = two_to_three(remaining_motion_2d)
+	# Calculate new velocity after colliding
+	var velocity_2d: Vector2 = three_to_two(velocity)
+	velocity_2d = velocity_2d.bounce(collision_normal_2d)
+	velocity = two_to_three(velocity_2d)
+	# Rotate to face new direction
+	look_at(global_transform.origin - velocity, Vector3(0, 1, 0))
+	# Finish travelling remaining distance
+	move_and_collide(remaining_motion_3d)
+
+
 func _physics_process(_delta: float):
-	move_and_slide(BULLET_SPEED * global_transform.basis.z)
+	var collision = move_and_collide(velocity)
+	if collision:
+		match collision.collider.name:
+			"Bullet":
+				pass
+			"Wall":
+				_collide_wall(collision)
+			"Tank":
+				pass
